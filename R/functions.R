d0c144af98a420ed1dcc44ca258f4b4aec9a2b03
Frederik <frederikbjerre@hotmail.com>
2025-12-03 10:00:55 +0100

    Convert descriptive stats to a function

---
+++ R/functions.R
@@ -0,0 +1,15 @@
+create_table_descriptive_stats <- function(data) {
+  #' Title
+  #'
+  #' @param data
+  #'
+  #' @returns
+  #' A data.frame/tibble
+  descriptive_table <- data |>
+    dplyr::group_by(metabolite) |>
+    dplyr::summarise(across(value, list(mean = mean, sd = sd))) |>
+    dplyr::mutate(across(where(is.numeric), \(x) round(x, digits = 1))) |>
+    dplyr::mutate(MeanSD = glue::glue("{value_mean} ({value_sd})")) |>
+    dplyr::select(Metabolite = metabolite, `Mean SD` = MeanSD)
+  return(descriptive_table)
+}
