d34747456b70b3fadd2f9625c2ab742218ba87e9
Frederik <frederikbjerre@hotmail.com>
2025-12-03 15:11:11 +0100

    Multiple: Add cleaning up the data roxygen commenting was misinterpreted. Move to right place

--- R/functions.R
+++ R/functions.R
@@ -1,10 +1,10 @@
-create_table_descriptive_stats <- function(data) {
   #' Create descriptive stats for each metabolite. Put it in a neat table
   #'
   #' @param data
   #'
   #' @returns
   #' A data.frame/tibble
+create_table_descriptive_stats <- function(data) {
   descriptive_table <- data |>
     dplyr::group_by(metabolite) |>
     dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) |>
@@ -14,12 +14,12 @@ create_table_descriptive_stats <- function(data) {
   return(descriptive_table)
 }
 
-create_plot_distributions <- function(data) {
   #' Visualises metabolite value distributions
   #'
   #' @param data
   #'
   #' @returns A plot object
+create_plot_distributions <- function(data) {
 
   data |>
     ggplot2::ggplot(
@@ -28,4 +28,17 @@ create_plot_distributions <- function(data) {
     ggplot2::geom_histogram() +
     ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free") +
     ggplot2::theme_minimal()
+}
+
+
+#' Clean up data mitigating redudancy
+#'
+#' @param data The lipidomics dataframe
+#'
+#' @returns a data.frame
+clean <- function(data) {
+  data |>
+    dplyr::group_by(dplyr::pick(-value)) |> # We group by all the columns except value because then we can keep all the columns
+    dplyr::summarise(value = mean(value), .groups = "keep") |>
+    dplyr::ungroup()
 }
